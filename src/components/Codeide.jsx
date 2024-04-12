c631e7b993441b784322852f2e8ecc50917ed121
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2024-04-12 07:21:23 +0530

    optimization

--- src/components/Codeide.jsx
+++ src/components/Codeide.jsx
@@ -20,7 +20,6 @@ const themeVariables = {
     '--button-text-color': '#FFFFFF',
     '--border-color': '#0077B6',
     '--output-bg-color': '#262626'
-    
   },
   dark: {
     '--background-color': '#262626',
@@ -32,7 +31,6 @@ const themeVariables = {
     '--output-bg-color': '#262626'
   }
 };
-
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -46,54 +44,41 @@ function App() {
   const fileInputRef = useRef(null);
   const isMobile = window.innerWidth <= 600;
 
-  
-
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
     Object.keys(themeVariables[theme]).forEach(property => {
       document.documentElement.style.setProperty(property, themeVariables[theme][property]);
     });
   }, [theme]);
-  const handleToggleTheme = () => setTheme(theme === "dark" ? "light" : "dark");
 
+  const handleToggleTheme = () => setTheme(theme === "dark" ? "light" : "dark");
 
   useEffect(() => {
     const guessLanguage = (code) => {
       if (/#include <(iostream|vector|string|map|set|algorithm)>|using namespace std;|class\s+\w+/.test(code)) {
         return "cpp";
       }
-      
       if ((/^#include <(stdio.h|stdlib.h|string.h|math.h|ctype.h|limits.h|assert.h)>/.test(code) ||
         /\b(printf|scanf|malloc|free)\b/.test(code)) && 
         !(/class\s+\w+|using namespace std;/.test(code))) {
       return "c";
     }
-
     if (/class\s+\w+\s*[{;].*public static void main\s*\(\s*String\s*\[\]\s+\w+\s*\)/s.test(code) || /^(package|import)\s+java\./m.test(code)) {
       return "java";
     }
-
-
     if (/(function\s+\w+\s*\(|\w+\s*=\s*function\s*\(|\w+\s*=>|document\.getElementById|console\.log)/.test(code)) {
       return "javascript";
     }
-
     if (/import\s+(\w+)(\s+as\s+\w+)?|from\s+\w+\s+import\s+\w+|print\s*\(/.test(code)) {
       return "python3";
     }
       return "undefined";
     };
-    
-    
-    
     setLanguage(guessLanguage(inputText));
   }, [inputText]);
 
-
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const options = {
       method: "POST",
       url: "https://online-code-compiler.p.rapidapi.com/v1/",
@@ -110,7 +95,6 @@ function App() {
       },
     };
 
-
     try {
       setIsRunning(true);
       const response = await axios.request(options);
@@ -118,7 +102,7 @@ function App() {
       setCpuTime(`${response.data.cpuTime}ms`);
       setMemory(`${response.data.memory}KB`);
       setOutput(response.data.output);
-      if (window.innerWidth <= 600) {
+      if (isMobile) {
         const outputArea = document.querySelector('.Output-area');
         outputArea.scrollIntoView({ behavior: 'smooth' });
       }
@@ -126,26 +110,17 @@ function App() {
       setIsRunning(false);
       console.error(error);
       setOutput("An error occurred while compiling the code.");
-
-
     }
   };
 
-  
   const getFileExtension = (language) => {
     switch (language) {
-      case "python3":
-        return "py";
-      case "c":
-        return "c";
-      case "cpp":
-        return "cpp";
-      case "java":
-        return "java";
-      case "javascript":
-        return "js";
-      default:
-        return "txt";
+      case "python3": return "py";
+      case "c": return "c";
+      case "cpp": return "cpp";
+      case "java": return "java";
+      case "javascript": return "js";
+      default: return "txt";
     }
   };
 
@@ -161,11 +136,8 @@ function App() {
       const fileExtension = file.name.split(".").pop();
       const mappedLanguage = mapFileExtensionToLanguage(fileExtension);
       setLanguage(mappedLanguage);
-
       const reader = new FileReader();
-      reader.onload = (e) => {
-        setInputText(e.target.result);
-      };
+      reader.onload = (e) => setInputText(e.target.result);
       reader.readAsText(file);
     }
   };
@@ -195,12 +167,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <select
-            id="language-select"
-            value={language}
-            onChange={(e) => setLanguage(e.target.value)}
-            className="Language-select"
-          >
+        <select id="language-select" value={language} onChange={(e) => setLanguage(e.target.value)} className="Language-select">
             <option value="c">C</option>
             <option value="python3">Python3</option>
             <option value="cpp">C++</option>
@@ -209,7 +176,7 @@ function App() {
           </select>
           <button className="option-button" onClick={handleToggleTheme}>
           {theme === "dark" ? <BsFillSunFill /> : <BsFillMoonStarsFill />} Toggle Theme
-        </button>        
+        </button>
         <button className="option-button" onClick={handleDownload}><FiDownload /> Download</button>
         <button className="option-button" id="run" onClick={handleSubmit} disabled={isRunning}>{isRunning ? "Running..." : "Run"} <BsPlayFill /></button>
         <input type="file" ref={fileInputRef} onChange={handleFileImport} style={{ display: "none" }} accept=".txt,.js,.py,.java,.cpp,.c" />
@@ -217,39 +184,24 @@ function App() {
       </header>
       <div className="App-body">
         <div className="left-column">
-        <Editor
-  height={isMobile ? "50vh" : "95vh"} 
-  language={getMonacoLanguageId(language)}
-  value={inputText}
-  onChange={setInputText}
-  theme={theme === "dark" ? "vs-dark" : "light"}
-  options={{
-    selectOnLineNumbers: true,
-    roundedSelection: false,
-    readOnly: false,
-    cursorStyle: "line",
-    automaticLayout: true,
-    highlightActiveIndentGuide: true,
-    autoClosingBrackets: "always",
-    autoClosingQuotes: "always",
-    autoIndent: "full",
-    formatOnType: true,
-    formatOnPaste: true,
-  }}
-/>
+          <Editor height={isMobile ? "50vh" : "95vh"} language={getMonacoLanguageId(language)} value={inputText} onChange={setInputText} theme={theme === "dark" ? "vs-dark" : "light"} options={{
+            selectOnLineNumbers: true, roundedSelection: false, readOnly: false, cursorStyle: "line", automaticLayout: true,
+            highlightActiveIndentGuide: true, autoClosingBrackets: "always", autoClosingQuotes: "always", autoIndent: "full",
+            formatOnType: true, formatOnPaste: true
+          }} />
         </div>
         <div className="right-column">
-          <Editor2 className="Editor2"value={inputValue} onValueChange={setInputValue} highlight={(code) => highlight(code, languages.js)} padding={10} placeholder="Enter input here "/>
+          <Editor2 className="Editor2" value={inputValue} onValueChange={setInputValue} highlight={(code) => highlight(code, languages.js)} padding={10} placeholder="Enter input here " />
           <div className="Output-area"><pre>{output}</pre></div>
           <div className="Statistics-area">
-          <div className="Stat-box">
-                <h3 className="Stat-title">Execution Time</h3>
-                <p className="Stat-value">{cpuTime}</p>
-              </div>
-              <div className="Stat-box">
-                <h3 className="Stat-title">Memory Usage</h3>
-                <p className="Stat-value">{memory}</p>
-              </div>
+            <div className="Stat-box">
+              <h3 className="Stat-title">Execution Time</h3>
+              <p className="Stat-value">{cpuTime}</p>
+            </div>
+            <div className="Stat-box">
+              <h3 className="Stat-title">Memory Usage</h3>
+              <p className="Stat-value">{memory}</p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './landingpage.css';
-import logo from './logo512.png'; // Import your logo
-import featureImage1 from './logo512.png'; // Import a relevant image for the Web IDE feature
-import featureImage2 from './logo512.png'; // Import a relevant image for the Code Editor feature
+import logo from './logo512.png'; 
+// import featureImage1 from './logo512.png'; 
+// import featureImage2 from './logo512.png'; 
 
 const LandingPage = () => {
   useEffect(() => {
@@ -19,11 +19,12 @@ const LandingPage = () => {
       <nav className="landing-nav">
         <div className="nav-brand">
           <img src={logo} alt="CodeCompiler logo" className="logo" />
-          CodeCompiler
+          WebCodeIde
         </div>
       </nav>
       <header className="landing-header" data-aos="fade-up">
         <h1 className="landing-title">Unleash Your Coding Potential</h1>
+        <br/>
         <p className="landing-subtitle">
           Write, compile, and execute your code in a powerful and intuitive environment.
         </p>
@@ -31,7 +32,7 @@ const LandingPage = () => {
       <section className="features-container">
         <div className="feature" data-aos="fade-right">
           <h2>Web IDE</h2>
-          <img src={featureImage1} alt="Web IDE" className="feature-image" />
+          {/* <img src={featureImage1} alt="Web IDE" className="feature-image" /> */}
           <p>
             Compile HTML, CSS, and JavaScript on the fly with our powerful Web IDE.
           </p>
@@ -41,7 +42,7 @@ const LandingPage = () => {
         </div>
         <div className="feature" data-aos="fade-left">
           <h2>Code Editor</h2>
-          <img src={featureImage2} alt="Code Editor" className="feature-image" />
+          {/* <img src={featureImage2} alt="Code Editor" className="feature-image" /> */}
           <p>
             Write code in your favorite programming languages with syntax highlighting
             and powerful features.
